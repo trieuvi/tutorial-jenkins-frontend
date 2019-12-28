63e81019a0270d6d8cf750e1e7791f8442461cf4
Trieu Nguyen <trieu@Trieu-Nguyen.attlocal.net>
2019-12-27 16:41:18 -0800

    v2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">App Version 1</h1>
+          <h1 className="App-title">App Version 2</h1>
         </header>
       </div>
     );
